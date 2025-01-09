d434bb5500f27f2c402a7d295e5fd45052a592ce
Rudolfs Magone <rudolfs.magone@28stone.com>
2025-01-09 18:02:58 +0200

    --added logos and settings screen

---
+++ core/src/main/java/io/github/practivce/screens/SettingsScreen.java
@@ -0,0 +1,198 @@
+package io.github.practivce.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+import io.github.practivce.gameClass.Drop;
+
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
+ */
+public class SettingsScreen implements Screen {
+    final Drop game;
+
+    Texture bgTexture;
+    Texture bucketTexture;
+    Texture dropTexture;
+    Sound dropSound;
+    Music music;
+    Sprite bucketSprite;
+    Vector2 touchPos;
+    Array<Sprite> dropSprites;
+    float dropTimer;
+    Rectangle bucketRectangle;
+    Rectangle dropRectangle;
+    AssetManager manager;
+    int dropsGathered;
+
+    public SettingsScreen(final Drop game) {
+        this.game = game;
+        bgTexture = new Texture("background.png");
+        bucketTexture = new Texture("bucket.png");
+        dropTexture = new Texture("drop.png");
+
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        music.setLooping(true);
+        music.setVolume(.5f);
+
+        bucketSprite = new Sprite(bucketTexture);
+        bucketSprite.setSize(1, 1);
+
+        touchPos = new Vector2();
+        bucketRectangle = new Rectangle();
+        dropRectangle = new Rectangle();
+        dropSprites = new Array<>();
+        manager = new AssetManager();
+
+        if (Gdx.files.internal("Spades/spades.atlas").exists()) {
+            Gdx.app.log("Info", "Atlas file exists!");
+        } else {
+            Gdx.app.log("Error", "Atlas file not found!");
+        }
+        manager.load("Spades/spades.atlas", TextureAtlas.class);
+    }
+
+    @Override
+    public void show() {
+        music.play();
+    }
+
+    @Override
+    public void render(float delta) {
+        if (manager.update()) { // Returns true when all assets are loaded
+            System.out.println("All assets loaded!");
+            input();
+            logic();
+            draw();
+        } else {
+
+            System.out.println("Loading progress: " + (manager.getProgress() * 100) + "%");
+        }
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        game.viewport.apply();
+        game.batch.setProjectionMatrix(game.viewport.getCamera().combined);
+        game.batch.begin();
+        float worldWidth = game.viewport.getWorldWidth();
+        float worldHeight = game.viewport.getWorldHeight();
+        game.batch.draw(bgTexture, 0, 0, worldWidth, worldHeight);
+        TextureAtlas textureAtlas = manager.get("Spades/spades.atlas", TextureAtlas.class);
+        TextureRegion reg = textureAtlas.findRegion("3");
+        game.batch.draw(reg, 0, 0, 2f, 3f);
+        bucketSprite.draw(game.batch);
+        for (Sprite dropSprite : dropSprites) {
+            dropSprite.draw(game.batch);
+        }
+
+        game.batch.end();
+    }
+
+    private void logic() {
+
+        float bucketWidth = bucketSprite.getWidth();
+        float bucketHeight = bucketSprite.getHeight();
+        float worldWidth = game.viewport.getWorldWidth();
+        float worldHeight = game.viewport.getWorldHeight();
+        float delta = Gdx.graphics.getDeltaTime(); // retrieve the current delta
+
+
+        bucketSprite.setX(MathUtils.clamp(bucketSprite.getX(), 0, worldWidth - bucketWidth));
+        bucketRectangle.set(bucketSprite.getX(), bucketSprite.getY(), bucketWidth, bucketHeight);
+
+        for (int i = dropSprites.size - 1; i >= 0; i--) {
+            Sprite dropSprite = dropSprites.get(i);
+            float dropWidth = dropSprite.getWidth();
+            float dropHeight = dropSprite.getHeight();
+
+            dropSprite.translateY(-2f * delta);
+            dropRectangle.set(dropSprite.getX(), dropSprite.getY(), dropWidth, dropHeight);
+
+            if (dropSprite.getY() < -dropHeight)
+                dropSprites.removeIndex(i);
+            else if (bucketRectangle.overlaps(dropRectangle)) {
+                dropsGathered++;
+                dropSprites.removeIndex(i);
+                dropSound.play(); // Play the sound
+            }
+        }
+
+        dropTimer += delta;
+        if (dropTimer > 1f) {
+            dropTimer = 0;
+            createDroplet();
+        }
+    }
+
+    private void input() {
+        float speed = 4f;
+        float delta = Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            bucketSprite.translateX(speed * delta);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            bucketSprite.translateX(-speed * delta);
+        }
+        if (Gdx.input.isTouched()) {
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY());
+            game.viewport.unproject(touchPos);
+            bucketSprite.setCenterX(touchPos.x);
+        }
+    }
+
+    private void createDroplet() {
+        float dropWidth = 1;
+        float dropHeight = 1;
+        float worldWidth = game.viewport.getWorldWidth();
+        float worldHeight = game.viewport.getWorldHeight();
+
+        Sprite dropSprite = new Sprite(dropTexture);
+        dropSprite.setSize(dropWidth, dropHeight);
+        dropSprite.setX(MathUtils.random(0f, worldWidth - dropWidth));
+        dropSprite.setY(worldHeight);
+        dropSprites.add(dropSprite);
+    }
+
+    @Override
+    public void dispose() {
+        bgTexture.dispose();
+        dropSound.dispose();
+        music.dispose();
+        dropTexture.dispose();
+        bucketTexture.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        game.viewport.update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+}
